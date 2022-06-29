4c4799c5face90b81b2ad77a9bf0ec86f80816b1
aosalaza12 <88208032+aosalaza12@users.noreply.github.com>
2022-06-29 14:59:42 -0400

    Update and rename MongoDB_test.py to mongodb_test.py

--- module_5/mongodb_test.py
+++ module_5/mongodb_test.py
@@ -3,4 +3,4 @@ from pymongo import MongoClient
 url = "mongodb+srv://admin:admin@cluster0.cptgd.mongodb.net/pytech"
 client = MongoClient(url)
 db = client.pytech
-print("Collection:", " ", db.list_collection_names)
+print("Collection:", " ", db.list_collection_names())
