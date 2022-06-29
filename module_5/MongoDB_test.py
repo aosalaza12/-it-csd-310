9f25e29667e57a57f21a5234c701346a9fa842cc
aosalaza12 <88208032+aosalaza12@users.noreply.github.com>
2022-06-28 21:18:18 -0400

    Update MongoDB_test.py

--- module_5/MongoDB_test.py
+++ module_5/MongoDB_test.py
@@ -1,1 +1,6 @@
-
+import  pymongo
+from pymongo import MongoClient
+url = "mongodb+srv://admin:admin@cluster0.cptgd.mongodb.net/pytech"
+client = MongoClient(url)
+db = client.pytech
+print("Collection:", " ", db.list_collection_names)
